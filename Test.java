214da8e67a4d73038d50c0dba8488326a31a9956
UserName324 <58950350+UserName324@users.noreply.github.com>
2023-06-21 16:02:20 +0200

    Test.java

--- Test.java
+++ Test.java
@@ -5,10 +5,11 @@ public class Test {
         //Elf e1 = new Elf("Elrond",80,20);
         //Elf e2 = new Elf("Legolas",70,24);
         Elf e1 = Elf.getInstance();
-        Elf e2 = Elf.getInstance();
+        Dvarf d1 = Dvarf.getInstance();
         System.out.println(e1);
-        System.out.println(e2);
-        e1.attack(e2);
-        System.out.println(e2);
+        System.out.println(d1);
+
+        e1.attack(d1);
+        System.out.println(d1);
     }
 }
